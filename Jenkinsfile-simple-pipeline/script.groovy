331f7dbe1d455a7128329948100c4e262e1da401
Abinash <root@studybuddy1>
2024-01-25 13:05:23 +0530

    update

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -8,9 +8,10 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'b4a8af34-f778-49ef-9c08-02a6ad8901e5', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t abinash8/myrepo:j-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push abinash8/myrepo:j-1.0 '
-    }
+        sh 'docker push abinash8/myrepo:j-1.0'
 } 
+
+}
 
 def deployApp() {
     echo 'deploying the application...'
